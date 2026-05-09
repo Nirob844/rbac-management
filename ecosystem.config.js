9cf648f128308b4b7bcf0aac6d42afddba6e91aa
Nirob Hossain <nirobhossainalip@gmail.com>
2026-05-09 15:35:03 +0600

    deploy error fix 4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'rbac-app',
-            script: 'dist/app.js',
+            script: 'dist/server.js',
             instances: 1,
             autorestart: true,
             watch: false,
